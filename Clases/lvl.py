507090fafb9edf7669075b1ea6f83a8f6d906193
daniproblematico <tabares.juan1403@gmail.com>
2023-02-09 14:18:19 -0500

    Actualizacion x

--- Clases/lvl.py
+++ Clases/lvl.py
@@ -1,3 +1,7 @@
+import json
+with open("project.geom", "r") as f:
+    datos=json.load(f)
+
 class Lvl:
     def __init__(self, id,H,cols,beams):
         self.id=id
@@ -6,9 +10,56 @@ class Lvl:
         self.beams=beams
 
 
-import json
-with open("project.geom", "r") as f:
-    datos=json.load(f)
+pisos = []
 
-for i in range(len(datos["pisos"])):
-    i=Lvl(enumerate(datos["pisos"])[i])
+for id in datos["pisos"]:
+    h= datos["pisos"][id]
+    col = list(datos["col_piso"][id].keys())
+    beam = list(datos["vig_piso"][id].keys())
+    piso = Lvl(id,h,col,beam)
+    pisos.append(piso)
+
+    
+
+class Beam(Lvl, id, node_i,node_f):
+    def __init__(self,):
+        x=0
+
+
+columnas = []
+
+
+
+
+
+
+
+
+
+
+
+
+#Se hace un for con cada piso
+for piso in pisos:
+    id= []
+    nodoS =[]
+    nodoF = []
+    #se hace un for con cada id de columna que se guarda en el objeto piso
+    for Col in piso.cols:
+        #agrega los ids de la columna por piso
+        id.append(Col)
+        #agrega los nodos iniciales de cada columna por piso
+        nodoS.append(list(datos["columnas"][Col])[0])
+        #agrega los nodos finales de cada columna por piso
+        nodoF.append(list(datos["columnas"][Col])[1])
+    #crea un nuevo objeto columna por piso. cada valor es un arreglo de todos los valores recolectados
+    col = columna(id,nodoS,nodoF)
+    #se agrega el conjunto de colimnas creado por piso. Es decir solo hay 3 elementos
+    columnas.append(col)
+
+#print(columnas[0].id)
+#print(columnas[1].id)
+print((columnas.len()))
+
+
+#col = Lvl.col(piso[1].cols,)
